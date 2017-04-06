e08c329e7449d26c259502b9ba01dd1a32730160
rana-abhi777 <abhisekrana777@gmail.com>
2017-04-06 17:22:38 +0530

    First commit second.py added

---
+++ second.py
@@ -0,0 +1,9 @@
+#this is a second py file to test the merge of 2 repos
+def looper(number):
+	for i in range(0,20):
+		print('This is ' + str(i) + ' times.')
+	print('Looper is finished now.')
+
+numberEntered = input('Enter a number : ')
+looper(numberEntered)
+
